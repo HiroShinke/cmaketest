9176e25f190fb5b53be59ac6c19eb71c3adcd948
Hirofumi SHINKE <hiro@molloy.local>
2021-07-11 17:11:46 +0900

    add sample tests

--- example.cpp
+++ example.cpp
@@ -2,12 +2,22 @@
 #include <gtest/gtest.h>
 
 
+int foo_add(int n, int m);
+int foo_minus(int n, int m);
+
 class Example : public ::testing::TestWithParam<int> { };
 
-TEST_P(Example, ExampleTest)
+TEST_P(Example, ExampleTest1)
 {
     int i = GetParam();
-    ASSERT_TRUE(i == i);
+    ASSERT_TRUE(foo_add(i, 1) == i + 1);
 }
+
+TEST_P(Example, ExampleTest2)
+{
+    int i = GetParam();
+    ASSERT_TRUE(foo_minus(i,1) == i - 1);
+}
+
 
 INSTANTIATE_TEST_CASE_P(ExampleCategory, Example, ::testing::Range(0, 2));
---
+++ foo.cpp
@@ -0,0 +1,12 @@
+
+
+#include <iostream>
+
+int foo_add(int n, int m) {
+  return n + m;
+}
+
+int foo_minus(int n, int m) {
+  return n - m;
+}
+
